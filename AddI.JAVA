0a1579c2451219da4508228d3ae3b622e395da3b
Allcharmian <91695211+Allcharmian@users.noreply.github.com>
2022-05-14 15:19:38 -0500

    Create AddI.JAVA

---
+++ AddI.JAVA
@@ -0,0 +1,6 @@
+// Creating a AddInterface interface
+import java.rmi.Remote;
+public interface AddInterface extends Remote {
+	// Declaring the method prototype
+	public int add(int x, int y) throws RemoteException;
+}
